61f436efcfaeee39752198be5b8799a9005edd81
gofiny <gofiny@inbox.ru>
2019-05-10 00:16:39 +0700

    new keys

--- project/config.py
+++ project/config.py
@@ -4,8 +4,8 @@ class WebhookConf:
     WEBHOOK_PORT = 443
     WEBHOOK_LISTEN = '217.182.21.102'
 
-    WEBHOOK_SSL_CERT = '/etc/letsencrypt/csr/0000_crt-certbot.pem'
-    WEBHOOK_SSL_KEY = '/etc/letsencrypt/keys/0000_key-certbot.pem'
+    WEBHOOK_SSL_CERT = '/keys/cert.pem'
+    WEBHOOK_SSL_KEY = '/keys/privkey.pem'
 
     WEBHOOK_URL_BASE = 'https://%s:%s' % (WEBHOOK_HOST, WEBHOOK_PORT)
     WEBHOOK_URL_PATH = '/%s/' % (API_TOKEN)
