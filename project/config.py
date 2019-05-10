70797a78a5e7796c9c79d5f54225bedb60d99882
Dmitry <gofiny@inbox.ru>
2019-05-10 15:56:31 +0700

    change keys

--- project/config.py
+++ project/config.py
@@ -4,8 +4,8 @@ class WebhookConf:
     WEBHOOK_PORT = 443
     WEBHOOK_LISTEN = '217.182.21.102'
 
-    WEBHOOK_SSL_CERT = '/keys/fullchain.pem'
-    WEBHOOK_SSL_KEY = '/keys/privkey.pem'
+    WEBHOOK_SSL_CERT = '/keys/cert.pem'
+    WEBHOOK_SSL_KEY = '/keys/priv.key'
 
     WEBHOOK_URL_BASE = 'https://%s:%s' % (WEBHOOK_HOST, WEBHOOK_PORT)
     WEBHOOK_URL_PATH = '/%s/' % (API_TOKEN)
