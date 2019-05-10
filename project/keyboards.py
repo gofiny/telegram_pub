897c994cba777e45ba029dae1f49e8f1a8d996c8
gofiny <gofiny@inbox.ru>
2019-05-10 22:57:49 +0700

    change inline keyboards

--- project/keyboards.py
+++ project/keyboards.py
@@ -14,11 +14,9 @@ class Keyboards:
     @staticmethod
     def subscribes():
         keyboard = types.InlineKeyboardMarkup()
-        keyboard.add(
-            types.InlineKeyboardButton(text='Мои подписки', callback_data='my_subs_info'),
-            types.InlineKeyboardButton(text='Начальная', callback_data='start_subs_info'),
-            types.InlineKeyboardButton(text='Прогрессивная', callback_data='progressive_subs_info'),
-            types.InlineKeyboardButton(text='Ультра', callback_data='ultra_subs_info')
-        )
-
+        keyboard.row(types.InlineKeyboardButton(text='Мои подписки', callback_data='my_subs_info'))
+        keyboard.row(types.InlineKeyboardButton(text='Начальная', callback_data='start_subs_info'))
+        keyboard.row(types.InlineKeyboardButton(text='Прогрессивная', callback_data='progressive_subs_info'))
+        keyboard.row(types.InlineKeyboardButton(text='Прогрессивная', callback_data='ultra_subs_info'))
+        
         return keyboard
