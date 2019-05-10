0bfcb6b4cfe1297b8b206b94271dac947ec29139
gofiny <gofiny@inbox.ru>
2019-05-10 23:20:46 +0700

    repair

--- project/models.py
+++ project/models.py
@@ -5,8 +5,8 @@ from datetime import datetime
 class Users(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     chat_id = db.Column(db.Integer, unique=True)
-    username = db.Column(db.String(50), default=None, null=True)
-    first_name = db.Column(db.String(50), null=True, default=None)
+    username = db.Column(db.String(50), default=None)
+    first_name = db.Column(db.String(50), default=None)
     reg_date = db.Column(db.DateTime, default=datetime.now())
 
     @staticmethod
