a4ab48d766f3407d1607ef8a5271b5d844a78d07
Dmitry <gofiny@inbox.ru>
2019-05-11 21:18:39 +0700

    refactor moderls

--- project/models.py
+++ project/models.py
@@ -5,17 +5,28 @@ from flask_security import UserMixin, RoleMixin
 user_subs = db.Table(
     'user_subs',
     db.Column('user_id', db.Integer, db.ForeignKey('users.id')),
-    db.Column('sub_id', db.Integer, db.ForeignKey('subscriptions.id')))
+    db.Column('sub_id', db.Integer, db.ForeignKey('subscriptions.id'))
+)
+
+user_roles = db.Table(
+    'user_roles',
+    db.Column('user_id', db.Integer(), db.ForeignKey('users.id')),
+    db.Column('role_id', db.Integer(), db.ForeignKey('role.id'))
+)
 
 
-class Users(db.Model):
+class Users(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     chat_id = db.Column(db.Integer, unique=True)
     username = db.Column(db.String(50), default=None)
     first_name = db.Column(db.String(50), default=None)
     reg_date = db.Column(db.DateTime)
+    email = db.Column(db.String(100), default=None, nullable=True)
+    password = db.Column(db.String(255), default=None, nullable=True)
+    active = db.Column(db.Boolean, default=0)
 
     subs = db.relationship('Subscriptions', secondary=user_subs, backref=db.backref('users', lazy='dynamic'))
+    roles = db.relationship('Role', secondary=user_roles, backref=db.backref('users', lazy='dynamic'))
 
     @staticmethod
     def get_user(chat_id):
@@ -41,25 +52,14 @@ class Subscriptions(db.Model):
         return f'{self.title}'
 
 
-# Security
-
-users_roles = db.Table(
-    'users_roles',
-    db.Column('user_id', db.Integer(), db.ForeignKey('SiteUsers.id')),
-    db.Column('role_id', db.Integer(), db.ForeignKey('Role.id'))
-)
-
-
-class SiteUsers(db.Model, UserMixin):
-    id = db.Column(db.Integer(), primary_key=True)
-    email = db.Column(db.String(100), unique=True)
-    password = db.Column(db.String(255))
-    active = db.Column(db.Boolean())
-    roles = db.relationship('Role', secondary=users_roles, backref=db.backref('site_users', lazy='dynamic'))
-
-
 class Role(db.Model, RoleMixin):
-    id = db.Column(db.Integer(), primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(30), unique=True)
     description = db.Column(db.String(255))
+
+    def __init__(self, *args, **kwargs):
+        super(Role, self).__init__(*args, **kwargs)
+
+    def __repr__(self):
+        return f'{self.name}'
 
