8926d7dd21238c208a21cbf4ddff632fdea036d9
Dmitry <gofiny@inbox.ru>
2019-05-11 12:35:11 +0700

    relationship between tables

--- project/models.py
+++ project/models.py
@@ -2,6 +2,12 @@ from app import db
 from datetime import datetime
 
 
+users_subs = db.Table(
+    'user_subs',
+    db.Column('user_id', db.Integer, db.ForeignKey('Users.id')),
+    db.Column('sub_id', db.Integer, db.ForeignKey('Subscription.id')))
+
+
 class Users(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     chat_id = db.Column(db.Integer, unique=True)
@@ -9,9 +15,11 @@ class Users(db.Model):
     first_name = db.Column(db.String(50), default=None)
     reg_date = db.Column(db.DateTime, default=datetime.now())
 
+    subs = db.relationship('Subscriptions', secondary=users_subs, backref=db.backref('users', lazy='dynamic'))
+
     @staticmethod
     def get_user(chat_id):
-        return Users.query.filter(Users.chat_id==chat_id).all()
+        return Users.query.filter(Users.chat_id==chat_id).first()
 
     def __init__(self, *args, **kwargs):
         super(Users, self).__init__(*args, **kwargs)
@@ -25,3 +33,9 @@ class Subscriptions(db.Model):
     title = db.Column(db.String(30))
     work_time = db.Column(db.Integer)  # Рабочее время подписки
     description = db.Column(db.Text)
+
+    def __init__(self, *args, **kwargs):
+        super(Subscriptions, self).__init__(*args, **kwargs)
+
+    def __repr__(self):
+        return f'<[{self.id}]Title: {self.title}>'
