3d1f92f79f72b6716f2c065848a09aff4bf5ff3b
gofiny <42298724+gofiny@users.noreply.github.com>
2021-01-20 21:03:10 +0700

    Update config.py

--- project/config.py
+++ project/config.py
@@ -1,23 +1,23 @@
 class WebhookConf:
-    API_TOKEN = '690204123:AAH2KDdj6S5If5TQkDvCANAIIJ-MBSfmkcE'
-    WEBHOOK_HOST = 's156888.hostiman.com'
+    API_TOKEN = ''
+    WEBHOOK_HOST = ''
     WEBHOOK_PORT = 443
-    WEBHOOK_LISTEN = '217.182.21.102'
+    WEBHOOK_LISTEN = ''
 
-    WEBHOOK_SSL_CERT = '/tel_pub/telegram_pub/project/ssl/server.crt'
-    WEBHOOK_SSL_KEY = '/tel_pub/telegram_pub/project/ssl/server.key'
+    WEBHOOK_SSL_CERT = ''
+    WEBHOOK_SSL_KEY = ''
 
     WEBHOOK_URL_BASE = 'https://%s:%s' % (WEBHOOK_HOST, WEBHOOK_PORT)
     WEBHOOK_URL_PATH = '/%s/' % (API_TOKEN)
 
-    PAYMENT_SECRET_WORD_ONE = 'vfct95uu'
-    PAYMENT_SECRET_WORD_TWO = 'ktxso9lo'
+    PAYMENT_SECRET_WORD_ONE = ''
+    PAYMENT_SECRET_WORD_TWO = ''
 
 
 class Configuration:
     DEBUG = True
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://telegram:telpass123@localhost/telegram'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = 'there are secret key'
-    SECURITY_PASSWORD_SALT = 'some salt'
+    SECRET_KEY = ''
+    SECURITY_PASSWORD_SALT = ''
     SECURITY_PASSWORD_HASH = 'bcrypt'
